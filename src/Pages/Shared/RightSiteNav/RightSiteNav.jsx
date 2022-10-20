b96931148795d989d1fb2344a28530a258944534
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-20 09:42:43 +0600

    rightsite bar almost done

--- src/Pages/Shared/RightSiteNav/RightSiteNav.jsx
+++ src/Pages/Shared/RightSiteNav/RightSiteNav.jsx
@@ -1,11 +1,50 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaTwitter,
+  FaWhatsapp,
+  FaTwitch,
+} from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
 
-const RightSiteNav = () => {
+const RightSideNav = () => {
   return (
     <div>
-      <h2>this is RightSiteNav</h2>
+      <ButtonGroup vertical>
+        <Button className="mb-2" variant="outline-primary">
+          {" "}
+          <FaGoogle></FaGoogle> Login with Google
+        </Button>
+        <Button variant="outline-dark">
+          {" "}
+          <FaGithub></FaGithub> Login with Github
+        </Button>
+      </ButtonGroup>
+      <div className="mt-4">
+        <h5>Find us on</h5>
+        <ListGroup>
+          <ListGroup.Item className="mb-2">
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaWhatsapp /> WhatsApp
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">
+            <FaTwitch /> Twitch
+          </ListGroup.Item>
+          <ListGroup.Item className="mb-2">Vestibulum at eros</ListGroup.Item>
+        </ListGroup>
+      </div>
+      <div></div>
     </div>
   );
 };
 
-export default RightSiteNav;
+export default RightSideNav;
