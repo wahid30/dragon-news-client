2c23d2f63a86ae8a441a17fbd8a62ce92fcfee2e
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-20 09:29:11 +0600

    server connected

--- src/Pages/Shared/LeftSiteNav/LeftSiteNav.jsx
+++ src/Pages/Shared/LeftSiteNav/LeftSiteNav.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const LeftSiteNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/news-category")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
     <div>
-      <h2>this is LeftSiteNav</h2>
+      <h4>All category: {categories.length}</h4>
     </div>
   );
 };
