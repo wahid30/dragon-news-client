b841222a5fec4ee27645a8b8e7c16bdb8e63f9e6
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-20 08:57:27 +0600

    bootstrap layout added

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Header from "../Pages/Shared/Header/Header";
+import LeftSiteNav from "../Pages/Shared/LeftSiteNav/LeftSiteNav";
+import RightSiteNav from "../Pages/Shared/RightSiteNav/RightSiteNav";
 
 const Main = () => {
   return (
     <div>
-      <Outlet></Outlet>
+      <Header></Header>
+      <Container>
+        <Row>
+          <Col lg="2">
+            <LeftSiteNav></LeftSiteNav>
+          </Col>
+          <Col lg="7">
+            <Outlet></Outlet>
+          </Col>
+          <Col lg="3">
+            <RightSiteNav></RightSiteNav>
+          </Col>
+        </Row>
+      </Container>
+      <Footer></Footer>
     </div>
   );
 };
