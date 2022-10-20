3bed317796cc90bb079900159d9837a81dc7125a
wahid30 <md.wahidulalamjp2@gmail.com>
2022-10-20 08:49:33 +0600

    shared compo added

---
+++ src/Pages/Shared/LeftSiteNav/LeftSiteNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeftSiteNav = () => {
+  return (
+    <div>
+      <h2>this is LeftSiteNav</h2>
+    </div>
+  );
+};
+
+export default LeftSiteNav;
---
+++ src/Pages/Shared/RightSiteNav/RightSiteNav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightSiteNav = () => {
+  return (
+    <div>
+      <h2>this is RightSiteNav</h2>
+    </div>
+  );
+};
+
+export default RightSiteNav;
